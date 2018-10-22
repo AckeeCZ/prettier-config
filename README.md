919a7883273c0145be79e13338e5a7513bc456ba
Jiří Čermák <jiri.cermak@ackee.cz>
2018-10-22 14:39:35 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Prettier configuration
+
+Prettier configuration used in Ackee frontend projects.
+
+```bash
+yarn add -D prettier-config-ackee
+
+// or
+
+npm install -D prettier-config-ackee
+```
---
+++ index.js
@@ -0,0 +1,11 @@
+'use strict';
+
+module.exports = {
+	singleQuote: true,
+	bracketSpacing: true,
+	semi: true,
+	arrowParens: 'avoid',
+	printWidth: 80,
+	jsxBracketSameLine: false,
+	trailingComma: 'all',
+};
